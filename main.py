bfdbaf3b4831a4e83e923470e4e6bb41143672ad
Raghav Sharma <92998665+rs0125@users.noreply.github.com>
2024-02-28 22:41:42 +0530

    Update main.py
    Addition of line count utility

--- main.py
+++ main.py
@@ -12,14 +12,33 @@ def open(file_name):
     #content of file dumped in different lines
     return lines
 
+def search(text, file_lines):
+    for i in file_lines:
+        temp = []
+        line_count = 0
+
+        #case sensitiveity via usage of '-i' in CL argument
+        if "-i" in sys.argv:
+            if text.lower() in i.lower():
+                temp.append(i)
+                line_count += 1
+        else:
+            if text in i:
+                temp.append(i)
+                line_count += 1
+
+        return (temp, line_count)
+
+
+#text to be searched and file name taken as input from the command line arg
 text = sys.argv[-2]
 file_name = sys.argv[-1]
 file_lines = open(file_name) 
 
-for i in file_lines:
-    if sys.argv[-3] == "-i":
-        if text.lower() in i.lower():
-            print(f"{i} at line {file_lines.index(i)}")
-    else:
-        if text in i:
-            print(f"{i} at line {file_lines.index(i)}")
+file_stats = search(text, file_lines)
+
+if "-l" in sys.argv:
+    print(f"line count of matches: {file_stats[1]}")
+else:
+    for i in file_stats[0]:
+        print(f"{i} at line {file_lines.index(i)}")
