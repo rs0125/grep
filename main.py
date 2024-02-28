8eb6b974f7415a6e4387613355e867b122a3cfc9
Raghav Sharma <92998665+rs0125@users.noreply.github.com>
2024-02-28 22:25:06 +0530

    Update main.py
    Case sensitivity implementation in search

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import sys
+import re
 
 def open(file_name):
+    #opens file, ends the program if the file does not exist
     try:
         file = open(file_name, "r")
     except:
         print("File doesn't exist")
         quit()
     lines = file.readlines()
+    #content of file dumped in different lines
     return lines
 
-text = sys.argv[1]
-file_name = sys.argv[2]
+text = sys.argv[-2]
+file_name = sys.argv[-1]
+file_lines = open(file_name) 
 
-file_lines = open(file_name)
 for i in file_lines:
-    if text in i:
-        print(i)
+    if sys.argv[-3] == "-i":
+        if text.lower() in i.lower():
+            print(f"{i} at line {file_lines.index(i)}")
+    else:
+        if text in i:
+            print(f"{i} at line {file_lines.index(i)}")
